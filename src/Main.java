51f311f6fb62b3afdc84a61df3dd0992f15538b4
EugenyVinogradov <vinogradov_e_v@rambler.ru>
2022-04-22 20:27:27 +0300

    init commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 230;
+        int countRublesForMile = 20;
+        int bonusMiles = ticketPrice / countRublesForMile;
+        System.out.println(bonusMiles);
+    }
+}
